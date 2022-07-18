3495fd82a1b1705a7c61ca56f8934e62eddeae50
Ankita Adhikrao Chavan <er.ankitachavan@gmail.com>
2022-07-18 12:27:13 +0530

    buy now on slider brings customer to product. payment form is done

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ const currentProductColors = document.querySelectorAll(".color");
 const currentProductSizes = document.querySelectorAll(".Size");
 
 
-menuItems.forEach((item, index) => {
+menuItems.forEach((item, index) => {         
     item.addEventListener("click", () => {
         //change the current slide
         wrapper.style.transform =`translateX(${-100 * index}vw)`;  //to slide our slider by 100vw to the next slider
@@ -102,14 +102,48 @@ menuItems.forEach((item, index) => {
         currentProductImg.src = choosenProduct.colors[0].img;
 
         //assign new colors
-        currentProductColors.forEach((color, index) => {
+        currentProductColors.forEach((color, index) => {     // new color options -> the two boxes of different color options
             color.style.backgroundColor = choosenProduct.colors[index].code;
         });
     });
 });
 
-currentProductColors.forEach((color,index)=>{
+currentProductColors.forEach((color,index)=>{         //this is to get each shoose in 2 options
     color.addEventListener("click", ()=>{
-        currentProductImg.src = choosenProduct.colors[index].img
-    })
-})
+        currentProductImg.src = choosenProduct.colors[index].img;
+    });
+});
+
+// currentProductSizes.forEach((size,index) => {         //to select size 42 43 44
+//     size.addEventListener("click", ()=>{
+//         currentProductSizes.forEach((size) => {        //this is for original state of button
+//             size.style.backgroundColor = "white";
+//             size.style.color = "black";
+//         });
+//             size.style.backgroundColor = "black";
+//             size.style.color = "white";
+//     });
+// });
+
+currentProductSizes.forEach((size, index) => {
+    size.addEventListener("click", () => {
+      currentProductSizes.forEach((size) => {
+        size.style.backgroundColor = "white";
+        size.style.color = "black";
+      });
+      size.style.backgroundColor = "black";
+      size.style.color = "white";
+    });
+  });
+
+  const productButton = document.querySelector(".productButton");
+  const payment = document.querySelector(".payment");
+  const close = document.querySelector(".close");
+  
+  productButton.addEventListener("click", () => {
+    payment.style.display = "flex";
+  });
+  
+  close.addEventListener("click", () => {
+    payment.style.display = "none";
+  });
